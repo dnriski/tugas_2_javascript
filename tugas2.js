a9544c50dd991ee265a784a8c930e72af5ac7079
dnriski <nugrohoriski159@gmail.com>
2023-02-09 17:57:28 +0700

    first commit

---
+++ tugas2.js
@@ -0,0 +1,13 @@
+const vokal = "aiueo";
+let angka = 100;
+var data = null;
+const konsonan = false;
+var data2 = undefined;
+var listArr = ["satu", "dua", "tiga"];
+
+console.log(vokal);
+console.log(angka);
+console.log(konsonan);
+console.log(data);
+console.log(data2);
+console.log(listArr);
